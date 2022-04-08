ea1ae1a5e6524594bf162f993777db539819c659
DRJingu <79426912+Lee-jin-gu@users.noreply.github.com>
2022-04-08 21:54:50 +0900

    Java

---
+++ Bronze 5/P_10998.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class P_10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf(String.valueOf(sc.nextBigInteger().multiply(sc.nextBigInteger())));
+    }
+}
