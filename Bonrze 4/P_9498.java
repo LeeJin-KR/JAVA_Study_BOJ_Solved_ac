da2d912c4d12df78fba60de67ff11790337d1e2a
LeeJin-KR <103107213+LeeJin-KR@users.noreply.github.com>
2022-04-12 18:59:47 +0900

    JAVA

---
+++ Bonrze 4/P_9498.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class P_9498 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("FFFFFFDCBAA".charAt(sc.nextInt()/10));
+    }
+}
