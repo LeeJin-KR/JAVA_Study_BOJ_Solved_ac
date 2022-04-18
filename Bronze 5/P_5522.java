112aba0c1f68693cc6e34c48c04a5b6597629228
DRJingu <79426912+Lee-jin-gu@users.noreply.github.com>
2022-04-18 22:50:17 +0900

    JAVA

---
+++ Bronze 5/P_5522.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P_5522 {
+    public static void main(String[] args) {
+        int temp =0;
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+
+            temp += sc.nextInt();
+        }
+        System.out.println(temp);
+    }
+}
---
+++ Bronze 5/P_7287.java
@@ -0,0 +1,5 @@
+public class P_7287 {
+    public static void main(String[] args) {
+        System.out.println("30\nlee_jin");
+    }
+}
